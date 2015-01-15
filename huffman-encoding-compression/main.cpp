547c7fd479c8e35e3d1e24e7fd50ca2b6c29fb6a
Karol Dzitkowski <k.dzitkowski@gmail.com>
2015-01-15 16:46:09 +0100

    Initial Commit

---
+++ huffman-encoding-compression/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  huffman-encoding-compression
+//
+//  Created by Karol Dzitkowski on 15.01.2015.
+//  Copyright (c) 2015 Karol Dzitkowski. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
